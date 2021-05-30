1a3a8472b0af7f623879d4afe6eb2404ed917a7b
Kim Yoo Kyung <tituvely@gmail.com>
2021-05-30 18:11:19 +0900

    JUnit Test code 작성

---
+++ src/main/java/org/example/springboot/Application.java
@@ -0,0 +1,17 @@
+package org.example.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// 스프링 부트의 자동 설정, 스프링 Bean 읽기와 생성을 모두 자동으로 설정
+// @SpringBootApplication이 있는 위치부터 설정을 읽어가기 때문에, 이 클래스는 항상 프로젝트의 최상단에 위치해야 함
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        // 내장 WAS를 실행 -> 톰캣을 설치할 필요가 없이, 스프링 부트로 만들어진 JAR파일을 실행하면 된다
+        // WHY? 내장 WAS를 사용하는 이유? -> 언제 어디서나 같은 환경에서 스프링 부트를 배포할 수 있기 때문
+        // 외장 WAS를 쓴다면 모든 서버는 WAS의 종류와 버전, 설정을 일치시켜야 한다
+        // HOW? 톰캣 없이 어떻게 배포하고 서비스를 할 수 있는가? ->
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -0,0 +1,39 @@
+package org.example.springboot.web;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+// 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함.
+@RunWith(SpringRunner.class)
+// 여러 스프링 노테이션 중, Web(Spring MVC)어에 집중할 수 있는 어노테이션.
+@WebMvcTest
+public class HelloControllerTest {
+
+    // 스프링이 관리하는 빈을 주입받음.
+    @Autowired
+    // 웹 API를 테스트할 때 사용함.
+    // 스프링 MVC 테스트의 시작점
+    // 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음
+    private MockMvc mvc;
+
+    @Test
+    public void hello가_리턴된다() throws Exception {
+        String hello = "hello";
+
+        // MockMvc를 통해 /hello 주소로 HTTP GET 요청을 함.
+        mvc.perform(get("/hello"))
+                // mvc.perform의 결과를 검증. 여기선 상태를 검증.
+                .andExpect(status().isOk())
+                // 응답 본문의 내용을 검증.
+                .andExpect(content().string(hello));
+    }
+
+}
