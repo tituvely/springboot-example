b8a612e05d1f8054fc2a3f31951e04e90718794a
Kim Yoo Kyung <tituvely@gmail.com>
2021-06-18 17:52:52 +0900

    어노테이션 기반으로 개선하기

---
+++ src/main/java/org/example/springboot/config/auth/LoginUser.java
@@ -0,0 +1,13 @@
+package org.example.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+// 어노테이션이 생성될 수 있는 위치를 지정
+// PARAMETER로 지정했으니, 메소드의 파라미터로 선언된 객체에서만 사용할 수 있음
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public class LoginUser {
+}
--- src/main/java/org/example/springboot/web/IndexController.java
+++ src/main/java/org/example/springboot/web/IndexController.java
@@ -10,6 +10,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.mail.Session;
 import javax.servlet.http.HttpSession;
 
 @RequiredArgsConstructor
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
 
         if(user != null) {
             model.addAttribute("userName", user.getName());
