a48dae9e4b144b52df29ebe7e5c38b5dc7408750
Kim Yoo Kyung <tituvely@gmail.com>
2021-06-18 20:01:54 +0900

    기존 테스트에 스프링 세큐리티 적용하

--- src/test/java/org/example/springboot/web/HelloControllerTest.java
+++ src/test/java/org/example/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package org.example.springboot.web;
 
+import org.example.springboot.config.auth.SecurityConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -14,7 +18,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 // 스프링 부트 테스트와 JUnit 사이에 연결자 역할을 함.
 @RunWith(SpringRunner.class)
 // 여러 스프링 노테이션 중, Web(Spring MVC)어에 집중할 수 있는 어노테이션.
-@WebMvcTest
+@WebMvcTest(controllers = HelloController.class,
+        excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE
+        , classes = SecurityConfig.class)
+        }
+)
 public class HelloControllerTest {
 
     // 스프링이 관리하는 빈을 주입받음.
@@ -24,6 +33,7 @@ public class HelloControllerTest {
     // 이 클래스를 통해 HTTP GET, POST 등에 대한 API 테스트를 할 수 있음
     private MockMvc mvc;
 
+    @WithMockUser(roles = "USER")
     @Test
     public void hello가_리턴된다() throws Exception {
         String hello = "hello";
@@ -36,6 +46,7 @@ public class HelloControllerTest {
                 .andExpect(content().string(hello));
     }
 
+    @WithMockUser(roles = "USER")
     @Test
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
