823253f6b4a0ae593affbfe9da790a7661c14576
Kim Yoo Kyung <tituvely@gmail.com>
2021-07-13 16:01:28 +0900

    profile API 추가

--- src/main/java/org/example/springboot/config/auth/SecurityConfig.java
+++ src/main/java/org/example/springboot/config/auth/SecurityConfig.java
@@ -26,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                     // URL, HTTP 메소드별로 관리가 가능함
                     // 지정된 URL들은 permitAll() 옵션을 통해 전체 열람 권한을 줌
                     // POST 메소드이면서, "/api/v1/**" 주소를 가진 API는 USER 권한을 가진 사람만 가능하도록 함
-                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**").permitAll()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console/**", "/profile").permitAll()
                     .antMatchers("/api/v1/**").hasRole(Role.USER.name())
                     // 설정된 값들 이외 나머지 URL들을 나타냄
                     // authenticated()을 추가하여 나머지 URL들은 모두 인증된 사용자들에게만 허용하게
